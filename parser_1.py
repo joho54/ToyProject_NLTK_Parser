5f43e478e7ce35f720687730102912706cfccfe5
bachohyeonho <133359989+bachohyeonho@users.noreply.github.com>
2024-06-07 17:48:17 +0900

    Arranged Code with clearly working CFG

--- parser_1.py
+++ parser_1.py
@@ -9,8 +9,6 @@ sentences = ['Could you show me the way to City Hall ?',
 'When she just finished cleaning her room, her mother called her.',
 'Which option do you want to select ?']
 
-sent = sentences[4]
-
 # (2) Make your CFG file for parsing your sentence.
 
 # ex. grammar.cfg (see the direction in the powerpoint)
@@ -22,11 +20,13 @@ import nltk
 grammar = nltk.data.load("file:.\\grammar.cfg")
 parser = nltk.ChartParser(grammar)
 
-for sent in sentences:
+for idx, sent in enumerate(sentences):
     tokens = nltk.tokenize.word_tokenize(sent)
     trees = parser.parse(tokens)
-
-    with open(".\\result.txt", "w") as f1:
+    
+    result_path = f'./sent{idx+1}_조현호.txt'
+    
+    with open(result_path, "w") as f1:
         for tree in trees:
             print(tree)
             f1.write(str(tree)+"\n\n")
